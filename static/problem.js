1392f7bcc9875a254bb1918f6fce30d12e19fa31
grchz <grqz.dev@proton.me>
2026-05-18 14:14:40 +1200

    upd

--- static/problem.js
+++ static/problem.js
@@ -1,19 +1,40 @@
 document.addEventListener('DOMContentLoaded', async e=>{
     const userCode = document.getElementById('user-code');
     const btn = document.getElementById('run');
+    const evalStdin = document.getElementById('eval-stdin');
     const output = document.getElementById('output');
-    let warned = false;
-    const outbuf = [];
-    const pyodide = await loadPyodide({
-        stdin: ()=>{ return 'dummy stdin'; },
-        stdout: l=>{ outbuf.push(l); output.innerText += (l + '\n'); },
-        stderr: l=>{ if (!warned) {warned = true; alert('stderr is ignored. Please use stdout instead!')} },
-    });
+    const pyodide = await loadPyodide({});
+    function setPyIO(py, o) {
+        py.setStdin({stdin: ()=>o[0]()});
+        py.setStdout({batched: l=>o[1](l)});
+        py.setStderr({batched: l=>o[2](l)});
+    }
+
+    const evalIO = [
+        function () { return this[3].shift(); },
+        function (l) { output.innerText += l + '\n'; },
+        function () { if (!this[5]) { this[5] = true; alert('stderr is ignored in judge environment. Please use stdout instead!'); } },
+        null,
+        null,
+        false,
+    ];
+
+    const judgeIO = [
+        function () { return this[3].shift(); },
+        function (l) { this[4].push(l); },
+        function () { if (!this[5]) { this[5] = true; alert('stderr is ignored in judge environment. Please use stdout instead!'); } },
+        [],
+        [],
+        false,
+    ];
+
+    setPyIO(pyodide, evalIO);
+    pyodide.setStderr({ batched: 0 })
     btn.removeAttribute('disabled');
     btn.innerText = 'Run';
     btn.onclick = ()=>{
+        evalIO[3] = evalStdin.innerText.split(/\r?\n/);
         output.innerText = '';
         pyodide.runPython(userCode.innerText);
     };
-    btn.classList.remove('hidden');
 });
