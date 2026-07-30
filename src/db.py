4c01bd6820e70365f781518c8c3f0802916bd136
grchz <grqz.dev@proton.me>
2026-07-30 23:10:26 +1200

    improve? db.py

--- src/db.py
+++ src/db.py
@@ -2,18 +2,23 @@ import contextlib
 import os
 import sqlite3
 import threading
-from typing import Callable, Concatenate, ParamSpec, TypeVar
+from typing import (
+    Callable,
+    Concatenate,
+    ParamSpec,
+    TypeVar,
+    cast,
+)
 
 P = ParamSpec('P')
 T = TypeVar('T')
 
 storage = threading.local()
 
-DB_PATH = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, 'app.db'))
-
 def global_db() -> sqlite3.Connection:
     if (db := getattr(storage, 'db', None)) is None:
-        setattr(storage, 'db', db := sqlite3.connect(DB_PATH))
+        setattr(storage, 'db', db := sqlite3.connect(os.path.abspath(os.path.join(
+                __file__, os.pardir, os.pardir, 'app.db'))))
         db.row_factory = sqlite3.Row
     return db
 
@@ -34,47 +39,50 @@ def get_cursor(cur: OptCursor = None):
     finally:
         cur.close()
 
-def opt_cursor(fn: Callable[Concatenate[sqlite3.Cursor, P], T]):
-    def inner(*a, cur: OptCursor = None, **kw) -> T:
+def opt_cursor(fn: Callable[Concatenate[sqlite3.Cursor, P], T]) -> Callable[P, T]:
+    def inner(*a: P.args, **k: P.kwargs) -> T:
+        cur = cast(OptCursor, k.pop('cur', None))
+        if k:
+            raise TypeError('kwargs other than "cur" are not allowed on db utils')
         with get_cursor(cur) as c:
-            return fn(c, *a, **kw)
+            return fn(c, *a, **k)
 
     return inner
 
 @opt_cursor
-def get_problems(c: sqlite3.Cursor) -> list[sqlite3.Row]:
+def get_problems(c: sqlite3.Cursor, *, cur: OptCursor = None) -> list[sqlite3.Row]:
     return c.execute(r'SELECT id, title FROM Problem').fetchall()
 
 @opt_cursor
-def problem_info(c: sqlite3.Cursor, p_id: int) -> sqlite3.Row | None:
+def problem_info(c: sqlite3.Cursor, p_id: int, *, cur: OptCursor = None) -> sqlite3.Row | None:
     return c.execute(r'SELECT id, title, "desc" FROM Problem WHERE id = ?', (p_id, )).fetchone()
 
 @opt_cursor
-def public_testcases(c: sqlite3.Cursor, p_id: int) -> list[sqlite3.Row]:
+def public_testcases(c: sqlite3.Cursor, p_id: int, *, cur: OptCursor = None) -> list[sqlite3.Row]:
     return c.execute(r'SELECT test_no, "in", "out", "note" FROM Testcase WHERE problem_id = ? AND type = 0', (p_id, )).fetchall()
 
 @opt_cursor
-def creds_of(c: sqlite3.Cursor, u_name: str) -> sqlite3.Row | None:
+def creds_of(c: sqlite3.Cursor, u_name: str, *, cur: OptCursor = None) -> sqlite3.Row | None:
     return c.execute(r'SELECT user_id, pw_hash, privilege_lvl FROM User WHERE name = ?', (u_name, )).fetchone()
 
 @opt_cursor
-def register(c: sqlite3.Cursor, u_name: str, pw_hash: str) -> None:
+def register(c: sqlite3.Cursor, u_name: str, pw_hash: str, *, cur: OptCursor = None) -> None:
     c.execute(r'INSERT INTO User (name, pw_hash, privilege_lvl) VALUES (?, ?, 1)', (u_name, pw_hash))
     c.connection.commit()
 
 @opt_cursor
-def submit(c: sqlite3.Cursor, u_id: int, p_id: int, result: int) -> None:
+def submit(c: sqlite3.Cursor, u_id: int, p_id: int, result: int, *, cur: OptCursor = None) -> None:
     c.execute(r'INSERT INTO Submission (user_id, problem_id, result) VALUES (?, ?, ?)', (u_id, p_id, result))
     c.connection.commit()
 
 @opt_cursor
-def get_subs(c: sqlite3.Cursor, u_id: int) -> list[sqlite3.Row]:
+def get_subs(c: sqlite3.Cursor, u_id: int, *, cur: OptCursor = None) -> list[sqlite3.Row]:
     return c.execute(r'SELECT problem_id, result FROM Submission WHERE user_id = ?', (u_id, )).fetchall()
 
 @opt_cursor
-def get_results(c: sqlite3.Cursor, p_id: int) -> list[sqlite3.Row]:
+def get_results(c: sqlite3.Cursor, p_id: int, *, cur: OptCursor = None) -> list[sqlite3.Row]:
     return c.execute(r'SELECT result FROM Submission WHERE problem_id = ?', (p_id, )).fetchall()
 
 @opt_cursor
-def get_userinfo(c: sqlite3.Cursor, u_id: int) -> sqlite3.Row:
+def get_userinfo(c: sqlite3.Cursor, u_id: int, *, cur: OptCursor = None) -> sqlite3.Row:
     return c.execute(r'SELECT name FROM User WHERE user_id = ?', (u_id, )).fetchone()
