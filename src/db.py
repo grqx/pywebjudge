84807eefa3e6068612246fd11d92e7daaa2fa70d
grchz <grqz.dev@proton.me>
2026-08-12 12:11:44 +1200

    improve db_util again

--- src/db.py
+++ src/db.py
@@ -10,10 +10,10 @@ from typing import (
     Protocol,
     TypeVar,
     cast,
+    overload,
 )
 
 P = ParamSpec('P')
-T = TypeVar('T')
 Tc = TypeVar('Tc', covariant=True)
 
 storage = threading.local()
@@ -42,75 +42,59 @@ def get_cursor(cur: OptCursor = None):
     finally:
         cur.close()
 
-class Decorated(Protocol[P, Tc]):
-    def __call__(*a: P.args, **k: P.kwargs) -> Tc: ...
-    mode: Literal['fetchall'] | Literal['fetchone'] | None
+FetchManyRet = list[sqlite3.Row]
+FetchOneRet = sqlite3.Row | None
+DBRet = FetchManyRet | FetchOneRet | None
 
-def db_util(query: LiteralString):
-    def wrapper(fn: Decorated[P, T]) -> Callable[P, T]:
-        def inner(*a: P.args, **k: P.kwargs) -> T:
+class Decorator(Protocol[Tc]):
+    @staticmethod
+    def __call__(cb: Callable[P, None], /) -> Callable[P, Tc]: ...
+
+@overload
+def db_util(query: LiteralString) -> Decorator[None]: ...
+@overload
+def db_util(query: LiteralString, mode: Literal['all']) -> Decorator[FetchManyRet]: ...
+@overload
+def db_util(query: LiteralString, mode: Literal['one']) -> Decorator[FetchOneRet]: ...
+
+def db_util(query: LiteralString, mode: Literal['all'] | Literal['one'] | None = None) -> Decorator[DBRet]:
+    def wrapper(_: Callable[P, None]) -> Callable[P, DBRet]:
+        def inner(*a: P.args, **k: P.kwargs) -> DBRet:
             cur = cast(OptCursor, k.pop('cur', None))
             if k:
                 raise TypeError('kwargs other than "cur" are not allowed on db utils')
             with get_cursor(cur) as c:
                 res = c.execute(query, a)
-                if fn.mode is not None:
-                    return cast(T, getattr(res, fn.mode)())
-                c.connection.commit()
-                return cast(T, None)
+                if mode is None:
+                    c.connection.commit()
+                    return None
+                return cast(DBRet, res.fetchall() if mode == 'all' else res.fetchone())
         return inner
     return wrapper
 
-FetchManyRet = list[sqlite3.Row]
-FetchOneRet = sqlite3.Row | None
-
-def fm_all(fn: Callable[P, T]) -> Decorated[P, FetchManyRet]:
-    ret = cast(Decorated[P, FetchManyRet], fn)
-    ret.mode = 'fetchall'
-    return ret
-
-def fm_one(fn: Callable[P, T]) -> Decorated[P, FetchOneRet]:
-    ret = cast(Decorated[P, FetchOneRet], fn)
-    ret.mode = 'fetchone'
-    return ret
-
-def fm_commit(fn: Callable[P, T]) -> Decorated[P, None]:
-    ret = cast(Decorated[P, None], fn)
-    ret.mode = None
-    return ret
-
-@db_util(r'SELECT id, title FROM Problem')
-@fm_all
+@db_util(r'SELECT id, title FROM Problem', 'all')
 def get_problems(*, cur: OptCursor = None): ...
 
-@db_util(r'SELECT id, title, "desc" FROM Problem WHERE id = ?')
-@fm_one
+@db_util(r'SELECT id, title, "desc" FROM Problem WHERE id = ?', 'one')
 def problem_info(p_id: int, *, cur: OptCursor = None): ...
 
-@db_util(r'SELECT test_no, "in", "out", "note" FROM Testcase WHERE problem_id = ? AND type = 0')
-@fm_all
+@db_util(r'SELECT test_no, "in", "out", "note" FROM Testcase WHERE problem_id = ? AND type = 0', 'all')
 def public_testcases(p_id: int, *, cur: OptCursor = None): ...
 
-@db_util(r'SELECT user_id, pw_hash, privilege_lvl FROM User WHERE name = ?')
-@fm_one
+@db_util(r'SELECT user_id, pw_hash, privilege_lvl FROM User WHERE name = ?', 'one')
 def creds_of(u_name: str, *, cur: OptCursor = None): ...
 
 @db_util(r'INSERT INTO User (name, pw_hash, privilege_lvl) VALUES (?, ?, 1)')
-@fm_commit
 def register(u_name: str, pw_hash: str, *, cur: OptCursor = None): ...
 
 @db_util(r'INSERT INTO Submission (user_id, problem_id, result) VALUES (?, ?, ?)')
-@fm_commit
 def submit(u_id: int, p_id: int, result: int, *, cur: OptCursor = None): ...
 
-@db_util(r'SELECT problem_id, result FROM Submission WHERE user_id = ?')
-@fm_all
+@db_util(r'SELECT problem_id, result FROM Submission WHERE user_id = ?', 'all')
 def get_subs(u_id: int, *, cur: OptCursor = None): ...
 
-@db_util(r'SELECT result FROM Submission WHERE problem_id = ?')
-@fm_all
+@db_util(r'SELECT result FROM Submission WHERE problem_id = ?', 'all')
 def get_results(p_id: int, *, cur: OptCursor = None): ...
 
-@db_util(r'SELECT name FROM User WHERE user_id = ?')
-@fm_one
+@db_util(r'SELECT name FROM User WHERE user_id = ?', 'one')
 def get_userinfo(u_id: int, *, cur: OptCursor = None): ...
